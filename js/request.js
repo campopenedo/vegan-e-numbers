a6e7973cee58b4c09246dd17fb82b775fa5f9814
Ezequiel M. Campo Penedo <campopenedo@protonmail.com>
2021-08-05 15:55:27 +0200

    Request

--- js/request.js
+++ js/request.js
@@ -11,23 +11,24 @@ export default function sendRequest(el){
         .then(data => data.json())
         .then(json => {
             let text = "";
-            json.forEach(element => {
-                let atr = "";
-                switch(element.vegan) {
-                    case "Vegan":
-                        atr = "vegan";
-                        break;
-                    case "Not Vegan":
-                        atr = "not-vegan";
-                        break;
-                    case "Depends of manufacter":
-                        atr="depends";
-                        break;
-                        
-                }
-                text += `<li>${element.enumber}: <span class='${atr}'>${element.vegan}</span>. Last update: ${element.last_update}</li>`;
-            });
-
+            if(el.target.value.length > 1) {
+                json.forEach(element => {
+                    let atr = "";
+                    switch(element.vegan) {
+                        case "Vegan":
+                            atr = "vegan";
+                            break;
+                        case "Not Vegan":
+                            atr = "not-vegan";
+                            break;
+                        case "Depends of manufacter":
+                            atr="depends";
+                            break;
+                            
+                    }
+                    text += `<li>${element.enumber}: <span class='${atr}'>${element.vegan}</span>. Last update: ${element.last_update}</li>`;
+                });
+            }
             document.getElementsByTagName("ul")[0].innerHTML = text;
         });
 }
